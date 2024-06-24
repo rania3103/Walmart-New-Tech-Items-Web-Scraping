d70af4b1f49e0d80c0e4451e9261d156b85ac8bb
Rania Abassi <135613385+rania3103@users.noreply.github.com>
2024-06-24 16:25:27 +0100

    add python files

---
+++ python_scripts/merge_files.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import pandas as pd
+import glob
+
+# Path to the current directory containing CSV files
+csv_files_path = '/csv_files/data*.csv'  # Adjust this pattern if necessary
+
+# Get a list of all CSV files in the current directory
+csv_files = glob.glob(csv_files_path)
+
+# Initialize an empty list to store each DataFrame
+dfs = []
+
+# Iterate over the list of CSV files
+for csv_file in csv_files:
+    # Read each CSV file into a DataFrame and append to `dfs`
+    df = pd.read_csv(csv_file)
+    dfs.append(df)
+
+# Concatenate all DataFrames in the list `dfs` into one DataFrame
+merged_df = pd.concat(dfs, ignore_index=True)
+
+# Save the merged DataFrame to a new CSV file
+merged_df.to_csv('merged_data.csv', index=False)
